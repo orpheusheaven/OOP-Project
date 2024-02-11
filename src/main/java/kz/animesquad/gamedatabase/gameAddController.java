dbadbbb39e24bcb7911c5a147d3a056ef1b18ed6
orpheusheaven <orpheusheaven@gmail.com>
2024-02-11 20:13:45 +0600

    pof

--- src/main/java/kz/animesquad/gamedatabase/gameAddController.java
+++ src/main/java/kz/animesquad/gamedatabase/gameAddController.java
@@ -11,6 +11,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Optional;
+import javafx.stage.Stage;
+
 
 public class gameAddController {
 
@@ -54,6 +56,8 @@ public class gameAddController {
         Optional<ButtonType> result = alert.showAndWait();
         if (result.isPresent() && result.get() == buttonTypeYes) {
             addToDatabaseAndList(title, developer, publisher, releaseYear, genre);
+            Stage stage = (Stage) btnAddGame.getScene().getWindow();
+            stage.close();
         }
     }
 
